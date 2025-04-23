4549902b7e9e041d72cc4e844047f338a5e35d0f
OlgaPegushina <pegushina.shine@yandex.ru>
2025-04-24 00:38:26 +0300

    ФЗ-14. Исправлен метод update в репозитории пользователя в случае изменения электронного адреса

--- src/main/java/ru/practicum/shareit/user/reposotory/UserRepositoryInMemoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/reposotory/UserRepositoryInMemoryImpl.java
@@ -46,6 +46,11 @@ public class UserRepositoryInMemoryImpl implements UserRepository {
     @Override
     public User update(User user) {
         Long userId = user.getId();
+        String checkEmail = users.get(userId).getEmail();
+        if (!checkEmail.equals(user.getEmail())) {
+            usersByEmail.remove(checkEmail);
+        }
+        usersByEmail.put(user.getEmail(), user);
         users.put(userId, user);
         return users.get(userId);
     }
