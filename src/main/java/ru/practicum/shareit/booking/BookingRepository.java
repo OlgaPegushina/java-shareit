74d8c64a07169ef3c9f1fb630a2abab1135d0e50
OlgaPegushina <pegushina.shine@yandex.ru>
2025-05-16 20:41:09 +0300

    ФЗ-15. форматирование пустых строк по чек-стайлу

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -9,39 +9,47 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    //findAllByItemOwnerId
     //ALL
     List<Booking> findAllByItemOwnerIdOrderByStart(Long ownerId, Sort sortOrder);
+
     //STAT Wait, reject
     List<Booking> findAllByItemOwnerIdAndStatusOrderByStart(Long ownerId, BookingStatus status, Sort sortOrder);
+
     //CURRENT
     List<Booking> findAllByItemOwnerIdAndStartLessThanEqualAndEndGreaterThanEqualOrderByStart(Long ownerId,
                                                                                               LocalDateTime now1,
                                                                                               LocalDateTime now2,
                                                                                               Sort sortOrder);
+
     //FUTURE
     List<Booking> findAllByItemOwnerIdAndStartAfterOrderByStart(Long ownerId, LocalDateTime now, Sort sortOrder);
+
     //PAST
     List<Booking> findAllByItemOwnerIdAndEndBeforeOrderByStart(Long ownerId, LocalDateTime now, Sort sortOrder);
 
-    //-----------------------------------------------------------------------------------------------------------------
-
+    //findAllByBookerId
     //ALL
     List<Booking> findAllByBookerIdOrderByStart(Long bookerId, Sort sortOrder);
+
     //WAITING
     List<Booking> findAllByBookerIdAndStatusOrderByStart(Long bookerId, BookingStatus status, Sort sortOrder);
+
     //REJECTED, CANCELED
     List<Booking> findAllByBookerIdAndStatusInOrderByStart(Long bookerId, List<BookingStatus> statuses, Sort sortOrder);
+
     //CURRENT
     List<Booking> findAllByBookerIdAndStartLessThanEqualAndEndGreaterThanEqualOrderByStart(Long bookerId,
                                                                                            LocalDateTime now1,
                                                                                            LocalDateTime now2,
                                                                                            Sort sortOrder);
+
     //FUTURE
     List<Booking> findAllByBookerIdAndStartAfterOrderByStart(Long bookerId, LocalDateTime now, Sort sortOrder);
+
     //PAST
     List<Booking> findAllByBookerIdAndEndBeforeOrderByStart(Long bookerId, LocalDateTime now, Sort sortOrder);
 
-    //-----------------------------------------------------------------------------------------------------------------
-
+    //other
     List<Booking> findAllByItemId(Long itemId);
 }
